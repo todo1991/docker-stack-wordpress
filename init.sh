ae2e3f38c1b974ab2f62a7516e809b925e71161f
todo1991 <tanqui101091@gmail.com>
2024-07-03 13:21:40 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -115,6 +115,7 @@ rm mycron
 echo "Cron job added to run ssl_renew.sh every day at 2AM."
 
 # done
+echo 'alias wpcli="docker compose run -ti --rm --no-deps --quiet-pull wpcli"' >> ~/.bash_aliases
 echo "I have completed my mission, in the process of erasing myself."
 rm init.sh
 echo "Have a nive day !!!"
